d61e96eef9d585fb8008fd15797941bc4a35bdea
kamuiii <kamuihatake1@gmail.com>
2026-02-09 15:16:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/playlist/53gTPoVMOMoH1LJ9TLr6ra?si=da5b3b64bc3a4e38", // Music streaming URL
+        musicUrl: "", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
